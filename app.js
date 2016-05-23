f144114c205f4d92c7aae48e7495083c3596dd35
MichaelRechenberg <mikerechenberg@owc.net>
2016-05-22 22:55:34 -0500

    Form is finished for the front page...need to do something useful with the data later (MongoDB) and use the csurf middleware to protect against CSRF.  Also need to validate input (check for SQL injections and the like)

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var router = express.Router();
 
 //Init middleware
 var bodyParser = require('body-parser');
+//all forms will be put in req.body.[input tag's name attribute]
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 var pug = require('pug');
 app.set('views', './views');
@@ -19,14 +22,20 @@ app.set('views', './views');
 //-----------ROUTING----------------//
 
 
-//all forms will be put in req.body.[input tag's name attribute]
-app.use(bodyParser.json());
 
 router.get('/', function(req, res){
     console.log('Front page reached');
 		res.render('frontpage.pug', {foo: 'Michael Rechenberg'});
 });
 
+router.post('/budde/', function(req, res){
+		console.log(req.body);
+		res.send("Thank you for your submission!");
+});
+
 app.use(router);
+
+//------------Starting Server----------//
+
 console.log("Listening on port " + PORTNUMBER);
 app.listen(PORTNUMBER);
